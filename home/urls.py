da39ea36726a5e11deac980dbda013eef6901835
Adnan Nazir <adnannazir.cs@gmail.com>
2023-12-19 16:10:51 +0500

    19-12-23 new2

--- home/urls.py
+++ home/urls.py
@@ -4,5 +4,11 @@ from . import views
 app_name = 'home'
 urlpatterns = [
     path('', views.index, name='dashboard'),
-    path('partial_rendering', views.partial_rendering, name='partial_rendering'),
+    # path('partial_rendering', views.partial_rendering, name='partial_rendering'),
+    #
+    # re_path(r'^partial_rendering/(?P<category_id>\d+)?/$', views.partial_rendering, name='partial_rendering'),
+
+    re_path(r'^partial_rendering/$', views.partial_rendering, name='partial_rendering'),
+    re_path(r'^partial_rendering/(?P<category_id>\d+)/$', views.partial_rendering,
+            name='partial_rendering_with_category'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,6 +1,9 @@
+from string import ascii_uppercase
+
 from django.core.paginator import Paginator
+from django.db.models import Q
 from django.http import HttpRequest, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 
 from dataclasses import dataclass
 
@@ -8,7 +11,8 @@ from django.views.decorators.http import require_GET
 from django_htmx.middleware import HtmxDetails
 from faker import Faker
 
-from home.models import Question
+from home.models import Question, Category
+# from django.db.models import BooleanField, Case, When
 
 
 # Create your views here.
@@ -18,9 +22,22 @@ class HtmxHttpRequest(HttpRequest):
 
 
 def index(request):
+    # categories = Category.objects.all()
+
+    # Retrieve and organize your categories
+    # categories = Category.objects.annotate(
+    #     has_children=Case(
+    #         When(children__isnull=False, then=True),
+    #         default=False,
+    #         output_field=BooleanField()
+    #     )
+    # ).order_by('has_children', 'order', 'name')  # Order by has_children first, then other fields
+
+    categories = Category.objects.order_by( 'order')
     context = {
         'parent': '',
-        'segment': 'dashboard'
+        'segment': 'dashboard',
+        'categories': categories,
     }
     return render(request, 'index.html', context)
 
@@ -36,13 +53,20 @@ people = [Person(id=i, name=faker.name()) for i in range(1, 235)]
 
 
 @require_GET
-def partial_rendering(request: HtmxHttpRequest) -> HttpResponse:
+def partial_rendering(request: HtmxHttpRequest, category_id=None) -> HttpResponse:
     # Retrieve questions from your database (example: getting all questions)
     questions = Question.objects.all()
+
+    if category_id:
+        # Filter questions by the provided category ID
+        category = get_object_or_404(Category, id=category_id)
+        questions = questions.filter(Q(categories=category))
+
     # Standard Django pagination
     page_num = request.GET.get("page", "1")
     page = Paginator(object_list=questions, per_page=10).get_page(page_num)
 
+    letters = {index: chr(65 + index - 1) for index in range(1, 27)}  # Generating letters A-Z
     # The htmx magic - use a different, minimal base template for htmx
     # requests, allowing us to skip rendering the unchanging parts of the
     # template.
@@ -57,5 +81,6 @@ def partial_rendering(request: HtmxHttpRequest) -> HttpResponse:
         {
             "base_template": base_template,
             "page": page,
+            'letters': letters,
         },
     )
